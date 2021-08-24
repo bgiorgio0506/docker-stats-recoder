50ce4bba75c5f2d1f857db91bed79ce4fa2ff3c7
Giorgio <bgiorgio0506@gmail.com>
2021-08-24 22:15:29 +0200

    fix csv export

--- main.ts
+++ main.ts
@@ -4,9 +4,8 @@ import fs from 'fs';
 import fsExtra from 'fs-extra';
 import path from 'path';
 
-const csv = require('csv');
+const ObjectsToCsv = require('objects-to-csv');
 var ON_DEATH = require('death'); //this is intentionally ugly
-var records:Array<Buffer>= [];
 const file = path.resolve('C:\\Users\\User\\Desktop\\Progetti\\POSCONDev\\docker-stats-recoder', 'test\\out\\test_data_1.csv');
 
 let stdOutArr:Array<IStats>= [];
@@ -21,7 +20,7 @@ function startTest() {
         throw err
       }
       else {
-          let splitStats:Array<string> = stdout.split(' ');
+          let splitStats:Array<string> = stdout.split('\t');
           stdOutArr.push({
             cpu: splitStats[0],
             memory: splitStats[1]
@@ -35,25 +34,10 @@ startTest();
 
 
  
-ON_DEATH(function(signal, err) {
+ON_DEATH(async function(signal, err) {
     console.log("Gracefully stopping CTRL+C");
-    csv.generate({
-      length: stdOutArr.length
-    }).on('readable', function(){
-      let record:Buffer;
-      while(record = this.read()){
-        records.push(record)
-      }
-    })
-    .on('error', function(err){
-      console.error(err)
-    })
-    .on('end', function(){
-      console.log(records, file)
+    const csv = new ObjectsToCsv(stdOutArr);
       fsExtra.ensureFileSync(file);
-      records.map((buffer) => {
-        fs.appendFileSync(file, buffer.toString('utf8'))
-      })
+      fsExtra.writeFileSync(file, await csv.toString());
       process.exit();
-    })
   });
